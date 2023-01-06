15fb3dc1e01e271674d597cb2880e7bca0aaa402
DjuricicS <djuricicsinisa@gmail.com>
2023-01-06 23:26:04 +0100

    clean the code base and add the authentication

--- Program.cs
+++ Program.cs
@@ -1,39 +1,26 @@
-using Starterkit.Data;
-using Starterkit._keenthemes;
-using Starterkit._keenthemes.libs;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpClient();
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<IKTTheme, KTTheme>();
-builder.Services.AddSingleton<IBootstrapBase, BootstrapBase>();
-
-IConfiguration configuration = new ConfigurationBuilder()
-                            .AddJsonFile("themesettings.json")
-                            .Build();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version())));
-var app = builder.Build();
-
-KTThemeSettings.init(configuration);
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace Authentication
 {
-    app.UseExceptionHandler("/Error");
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-
-app.UseStaticFiles();
-
-app.UseRouting();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
---
+++ README.md
@@ -0,0 +1,37 @@
+# Authenticate a Blazor server application
+
+This example demonstrates how to authenticate a Blazor server application.
+
+## Prerequisites
+
+- [.NET Core 3.1 SDK or later](https://dotnet.microsoft.com/download/dotnet-core)
+- [Visual Studio tool](https://visualstudio.microsoft.com/vs/) with installed “ASP.NET and web development”
+
+## To run the application
+
+* Clone the repository using below command line from the command prompt.
+
+```
+git clone https://github.com/SyncfusionExamples/blazor-authentication
+```
+
+* Navigate to the specified application folder.
+
+```
+cd blazor-authentication
+```
+
+* Run the application using below command line.
+
+```
+dotnet run
+```
+
+* Copy/paste the localhost link in the browser to launch the application.
+
+## Blog reference
+The detailed information about this is available in the blog, [Easy Steps to Create a Blazor Server-Side App with Authentication](https://www.syncfusion.com/blogs/post/easy-steps-create-a-blazor-server-side-app-with-authentication.aspx)
+
+## See also
+* [Online examples](https://blazor.syncfusion.com)
+* [Documentation](https://blazor.syncfusion.com/documentation/introduction/)
